30f8016a10f44579578dbb4b106ba21839dfa542
Rajit Dang <r3dang@ucsd.edu>
2016-12-16 12:00:45 +0530

    Added Slider functionality

--- EasyTip/ViewController.swift
+++ EasyTip/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var customTipLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,24 +27,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /**
+     * Function binded to view. When one taps,
+     * keyboard closes
+     */
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
+    /**
+     * When bill amount is changed, calculate Tip
+     */
     @IBAction func billChanged(_ sender: Any) {
         calculateTip()
     }
     
+    /**
+     * When segmented control option is changed, recalculate tip
+     */
     @IBAction func differentTip(_ sender: Any) {
         calculateTip()
     }
     
+    @IBAction func customTipAmount(_ sender: Any) {
+        customTipLabel.text = String(format: "%0.2f", tipSlider.value)
+        calculateTip()
+    }
+    
+    /**
+     * main driver function which dynamically calculates tip
+     */
     func calculateTip() {
         let percentages = [0.18, 0.20, 0.25]
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * percentages[tipControl.selectedSegmentIndex]
+        var tip = 0.0
+        if tipControl.selectedSegmentIndex == 3 {
+            tip = bill * Double(tipSlider.value/100)
+        } else {
+            tip = bill * percentages[tipControl.selectedSegmentIndex]
+        }
+            
         let total = bill + tip
-        
         tipLabel.text = String(format: "$%0.2f", tip)
         totalLabel.text = String(format: "$%0.2f", total)
     }
